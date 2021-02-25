e3e8022e21c55e11e14794358703a058765d0229
Isaque Rocha <isaquerocha@gmail.com>
2021-02-25 19:56:19 -0300

    ci: change ci config to decrypt secret file again

--- .github/workflows/decrypt_secret.sh
+++ .github/workflows/decrypt_secret.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
+# Decrypt the file
+mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_SECRET_PASSPHRASE" \
 --output ./storage/credentials/google/laravel-fc-microservice.json laravel-fc-microservice.json.gpg
