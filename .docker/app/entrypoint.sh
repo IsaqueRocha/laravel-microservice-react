1a0f897b12c7c9d3ea8953505feb280c156a84b1
Isaque Rocha <isaquerocha@gmail.com>
2021-03-24 17:36:44 -0300

    chore: backend code reconfigured

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-# chown -R www-data:www-data .
+
+### FRONT-END
+npm config set cache /var/www/.npm-cache --global
+cd frontend && npm install & cd ..
+
+### BACK-END
+cd backend
+chown -R www-data:www-data .
+
+if [ ! -f ".env" ]; then
+    cp .env.exmaple .env
+fi
+
 composer install
 php artisan key:generate
 php artisan migrate
